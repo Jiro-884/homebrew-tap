b7d6030e5f1ae9930711d69d73de8918324e2769
Jiro-884 <166469311+Jiro-884@users.noreply.github.com>
2024-07-18 09:55:19 +0900

    Update blago.rb

--- Fomula/blago.rb
+++ Fomula/blago.rb
@@ -1,9 +1,9 @@
 VERSION="0.1.1"
 PRODUCT="blago"
 
-class flexpress < Formula
+class blago < Formula
   desc "A tool for archiving files and directories and extracting several archive formats."
-  homepage "https://github.com/SASA-AKITO/flexpress"
+  homepage "https://github.com/Jiro-884/blago"
   version VERSION
   license "MIT"
 
@@ -11,27 +11,27 @@ class flexpress < Formula
   depends_on "bash-completion@2" => :optional
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/SASA-AKITO/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_amd64.tar.gz"
+    url "https://github.com/Jiro-884/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_amd64.tar.gz"
     sha256 "0fc84ded2d5a630c5d6c18781e96c03156e81e5be06a214cf018d46866bbe37c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/SASA-AKITO/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_arm64.tar.gz"
+    url "https://github.com/Jiro-884/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_arm64.tar.gz"
     sha256 "231d31f06856146b6cee6829e577a8e89f178b522829aa725a3c6ad73b14dac0"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/SASA-AKITO/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_amd64.tar.gz"
+    url "https://github.com/Jiro-884/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_amd64.tar.gz"
     sha256 "8a6ca11f56e2f24f5de0fd853d80e6e735bd76dd6985cecc6dd3257c1473f46a"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/SASA-AKITO/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_arm64.tar.gz"
+    url "https://github.com/Jiro-884/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_arm64.tar.gz"
     sha256 "8f84ea88797ad7e1b6f872be227211da2722c31c1a8b7acc80079028631c922c"
   end
 
   def install
-    bin.install "flexpress"
+    bin.install "blago"
 
     bash_completion.install "completions/bash/sibling.bash" if build.with? "completions"
     fish_completion.install "completions/fish/sibling.bash" if build.with? "completions"
@@ -39,6 +39,6 @@ class flexpress < Formula
   end
 
   test do
-    system "#{bin}/flexpress --version"
+    system "#{bin}/blago --version"
   done
 end
